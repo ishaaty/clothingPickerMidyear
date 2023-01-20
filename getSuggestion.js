14c0da4d2d7fac1d89741d615f5dd66b3dab3630
Julia Thompson <jewelst2014@gmail.com>
2023-01-20 09:59:00 -0500

    Started setting up file to handle user input

--- getSuggestion.js
+++ getSuggestion.js
@@ -3,5 +3,8 @@ const enterBtn = document.querySelector("button");
 enterBtn.addEventListener("click", getClothes);
 const testBtn = document.getElementById("test");
 function getClothes() {
-    console.log("It worked!");
+    let indieInput = document.getElementById("indie");
+    if (indieInput.checked == true) {
+        console.log("It worked!");
+    }
 }
--- getSuggestion.ts
+++ getSuggestion.ts
@@ -1,7 +1,35 @@
 const enterBtn = document.querySelector("button") as HTMLButtonElement;
-enterBtn.addEventListener("click", getClothes);
 const testBtn = document.getElementById("test") as HTMLParagraphElement;
+let userAesthetic;
+let userClothingType;
+let userColor;
+let userPrice;
+
+enterBtn.addEventListener("click", getClothes);
 
 function getClothes() {
-    console.log("It worked!");
+    userAesthetic = getAesthetic();
+    userClothingType = getClothingType();
+    userColor = getColor();
+    userPrice = getPrice();
+}
+
+function getAesthetic() {
+    let indieInput = document.getElementById("indie") as HTMLInputElement;
+    if (indieInput.checked == true) {
+        return 
+    }
+}
+
+function getClothingType() {
+
+}
+
+function getColor() {
+
+}
+
+
+function getPrice() {
+
 }
