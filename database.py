a0d78c0fcb788bdba328b294156655583e5d3270
ishaaty <ishatyagi888@gmail.com>
2023-01-19 19:13:24 -0500

    fixed database files

--- database.py
+++ database.py
@@ -1,10 +1,17 @@
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost",
-                     user="root",
-                     password="",
-                     db="midyearProject")
+db = MySQLdb.connect(host="sqlclassdb-instance-1.cqjxl5z5vyvr.us-east-2.rds.amazonaws.com",
+                     user="clothing",
+                     password="5kjhadkjh45saljf",
+                     db="midyear_2223_clothing")
 
 mycursor = db.cursor()
 
-mycursor.execute("select * from colors")
+mycursor.execute("select color_name from colors where color_id = 5")
+
+
+myresult = str(mycursor.fetchall())
+
+print(myresult.strip("(").strip(",").strip(")"))
+print(myresult)
+db.close()
