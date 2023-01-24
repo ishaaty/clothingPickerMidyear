5e1824d8c6d50f75760467d17a6d9fbf74cfdb1c
ishaaty <ishatyagi888@gmail.com>
2023-01-24 07:40:03 -0500

    idk what i did

--- db/db_init.js
+++ db/db_init.js
@@ -2,7 +2,7 @@ const db = require("./db_connection");
 
 /**** Read the sample items inserted ****/
 
-const read_stuff_table_sql = "SELECT * FROM colors";
+const read_stuff_table_sql = "SELECT * FROM colors where color_name like 'b%'";
 
 db.execute(read_stuff_table_sql, 
     (error, results) => {
@@ -11,6 +11,7 @@ db.execute(read_stuff_table_sql,
 
         console.log("Table 'colors' initialized with:")
         console.log(results);
+        console.log(results[0].color_name);
     }
 );
 
