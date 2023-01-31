f8966fa1eb61864242ff2ef738c8c35b963d0664
ishaaty <ishatyagi888@gmail.com>
2023-01-31 10:59:13 -0500

    yay

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const app = express();
 const port = 8080;
 const logger = require("morgan");
 const db = require('./public/db/db_connection');
+const bp = require('body-parser');
+const aesthetics = ["Indie", "Cottagecore", "Grunge"]
 
 // define middleware that logs all incoming requests
 app.use(logger("dev"));
 app.use(express.static(__dirname + '/public'));
-
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
 // define a route for the default home page
 app.get( "/", ( req, res ) => {
@@ -27,16 +30,23 @@ app.get( "/survey", ( req, res ) => {
     res.sendFile( __dirname + "/pages/survey.html" );
 } );
 
-app.get( "/survey/result", ( req, res ) => {
+const main_query =  `select link, img_name from items_xref as ix, links, imgs, aesthetics, prices, clothing_type as ct, colors 
+                    where ix.aesthetic_id = aesthetics.aesthetic_id and ?  
+                     and ix.type_id = ct.type_id and ? 
+                     and ix.price_id = prices.price_id and prices.price_val <= ? 
+                     and (ix.color1_id = colors.color_id or ix.color2_id = colors.color_id or ix.color3_id = colors.color_id) 
+                     and ? 
+                     and ix.img_id = img.img_id and ix.link_id = links.link_id 
+                     order by img_name";`
+
+
+app.post("/survey/result", (req, res)=>{
+    console.log(req.body.Indie);
+    console.log(req.body.Cottagecore);
     res.sendFile( __dirname + "/pages/result.html" );
-    // let stuff = getClothes();
-    // db.execute(main_query, (error, results) => {
-    //     if (error)
-    //         res.status(500).send(error); // Internal Server Error
-    //     else
-    //         res.send(results);
-    // });
-} );
+
+});
+
 
 // start the server
 app.listen( port, () => {
