6a37ccd3bd663fd43dff7c2b7dbd39122f540dcb
Julia Thompson <jewelst2014@gmail.com>
2023-02-02 10:01:46 -0500

    Fixed for loop, need to fix undefined

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require("morgan");
 const db = require('./public/db/db_connection');
 const bp = require('body-parser');
 const aesthetics = ["Indie", "Cottagecore", "Grunge", "Monochromatic", "Dark-Academia", "Light-Academia"]
+let selectedAesthetics = [];
 
 // define middleware that logs all incoming requests
 app.use(logger("dev"));
@@ -43,10 +44,10 @@ const main_query =  `select link, img_name from items_xref as ix, links, imgs, a
 app.post("/survey/result", (req, res)=>{
     console.log(req.body.Indie);
     console.log(req.body.Cottagecore);
-    for (let i = 0; i < aesthetics; i++) {
-        // if (req.body.aesthetics[i]) {
+    for (let i = 0; i < aesthetics.length; i++) {
+        if (req.body.aesthetics[i] == 'on') {
             selectedAesthetics.push(aesthetics[i]);
-        // }
+        }
     }
     console.log(selectedAesthetics);
     res.sendFile( __dirname + "/pages/result.html" );
